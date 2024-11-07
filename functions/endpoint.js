28c67760c3a0174a48c8d24cd7d011bf896e9214
Ebina Kai <85666313+EbinaKai@users.noreply.github.com>
2024-11-07 11:10:48 +0200

    [update] support not only email.

---
+++ functions/endpoint.js
@@ -0,0 +1,76 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+};
+
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+    });
+}
+
+const db = admin.firestore();
+
+exports.handler = async (event, context) => {
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS'
+    };
+
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers
+        };
+    }
+
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ message: 'Method Not Allowed' }),
+            headers,
+        };
+    }
+
+    try {
+        const { data } = JSON.parse(event.body);
+
+        if (!data) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: 'Payload is required' }),
+                headers,
+            };
+        }
+
+        const dataToSave = {};
+        data.forEach((item, index) => {
+            dataToSave[`item${index + 1}`] = item;
+        });
+
+        await db.collection('data').add(dataToSave);
+
+        return {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({ message: 'Request successfully' }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            headers,
+            body: JSON.stringify({ error: error.message })
+        };
+    }
+};
